d53aa6cc67595fbd1f8c141b6f59092987a04e14
Zilby <azilby@gmail.com>
2017-11-27 00:51:47 -0500

    Added base of project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Used for controlling all aspects of the player controlled sphere. 
+/// </summary>
+public class PlayerController : MonoBehaviour {
+
+	private bool rotation;
+
+	const float rotationSpeed = 5;
+
+	void Start () {
+		Rotate(true);
+		StartCoroutine(Rotate());
+	}
+
+	private void Rotate(bool right) {
+		rotation = right;
+	}
+
+	private IEnumerator Rotate() {
+		for (;;)
+		{
+			transform.Rotate(0, 0, rotation ? -rotationSpeed : rotationSpeed);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
