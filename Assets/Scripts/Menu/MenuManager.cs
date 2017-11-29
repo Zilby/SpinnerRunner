9ec7eef473c8d65ef7da98723ef1e8dce6070160
Zilby <azilby@gmail.com>
2017-11-29 05:03:43 -0500

    Added menu and fonts

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public Button play;
+
+	// Use this for initialization
+	void Start () {
+		play.onClick.AddListener(LoadGame);
+	}
+	
+	private void LoadGame() {
+		SceneManager.LoadScene("Game");
+	}
+}
---
+++ Assets/Scripts/Menu/MenuSpinner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSpinner : MonoBehaviour {
+
+	/// <summary>
+	/// The rotation speed.
+	/// </summary>
+	const float ROTATION_SPEED = 0.5f;
+
+	private void Start()
+	{
+		StartCoroutine(Rotate());
+	}
+
+	/// <summary>
+	/// Rotates this object.
+	/// </summary>
+	private IEnumerator Rotate()
+	{
+		for (;;)
+		{
+			transform.Rotate(0, 0, -ROTATION_SPEED);
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
